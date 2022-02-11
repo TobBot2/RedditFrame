c4bbc02389ac44b112ddc281305f2d4293ef8aa2
TobBot2 <38795143+TobBot2@users.noreply.github.com>
2022-02-11 08:43:46 -0500

    Improved Dithering

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import best_reddit
 import process_image
 import time
 
-def main(index):
+def main(index = None):
     subreddit = best_reddit.get_subreddit(index)
     print('Subreddit chosen:', subreddit)
-    posts = best_reddit.get_top_n_json(subreddit, 15, 'week')
+    posts = best_reddit.get_top_n_json(subreddit, 10, 'day')
 
     post = None
     for candidate_post in posts:
@@ -16,19 +16,22 @@ def main(index):
         print('Post chosen:', post['title'], post['img_url'])
         try:
             process_image.save_image_to_file(post['img_url'])
-            process_image.resize_and_crop((480, 800))
+            process_image.resize_with_bars((480, 800))
             process_image.FS_dithering()
             time.sleep(5)
         except Exception as e:
             print('failed to get, resize or dither image', e)
 
 def loop_all(start_at = 0):
-    with open('subreddits.csv') as f:
+    with open('subreddits.csv', 'r') as f:
         length = len(f.read().split(','))
     
     for i in range(length - start_at):
         main(i + start_at)
         time.sleep(5)
+def loop_random(times):
+    for i in range(times):
+        main()
 
 if __name__ == "__main__":
-    loop_all()
+    loop_random(20)
--- process_image.py
+++ process_image.py
@@ -1,48 +1,75 @@
 import math
 import requests
 # import bs4
-from PIL import Image
+from PIL import Image, ImageOps
 
 def save_image_to_file(url):
     raw_img = requests.get(url).content
     with open('cuteness.png', 'wb') as f:
         f.write(raw_img)
 
-    # soup = bs4.BeautifulSoup(requests.get(url).text, 'html.parser')
-    # img_url = soup.find_all('img')[0].get('src')
-    # raw_img = requests.get(img_url).content
-    # with open('cuteness.png', 'wb') as f:
-    #     f.write(raw_img)
-
 def resize_and_crop(new_size):
-    # TODO size to fit width or height (whatever is smaller) to new_size, then crop the other to be also new_size while keeping the image centered
     with Image.open('cuteness.png') as img:
         size = img.size
-        aspect = size[1] / size[0] # y/x
-        new_aspect = new_size[1] / new_size[0]
-        if aspect > new_aspect:
+        aspect = size[0] / size[1] # x/y
+        new_aspect = new_size[0] / new_size[1]
+        if aspect < new_aspect:
             # crop vertically
-            source_box = (0, size[1]/2 - size[0]/2, size[0], size[0] * aspect / new_aspect)
+            crop_h = size[0] / new_aspect
+            source_box = (0, size[1]/2 - crop_h/2, size[0], size[1]/2 + crop_h/2)
         else:
             # crop horizontally
-            source_box = (size[0]/2 - size[1]/2, 0, size[1] * aspect / new_aspect, size[1])
+            crop_w = size[1] * new_aspect
+            source_box = (size[0]/2 - crop_w/2, 0, size[0]/2 + crop_w/2, size[1])
+
         new_img = img.resize((new_size[0], new_size[1]), Image.BILINEAR, source_box)
         new_img.save('cuteness.png')
-    
+
+def resize_with_bars(new_size):
+    with Image.open('cuteness.png') as img:
+        size = img.size
+        aspect = size[0] / size[1]
+        new_aspect = new_size[0] / new_size[1]
+        if (aspect > new_aspect): # increasing size, so flip the check
+            # expand vertically
+            border = int((size[0] - size[1] * new_aspect) /2)
+            img = ImageOps.expand(img, (0, border), (0, 0, 0))
+        else:
+            # expand horizontally
+            border = int((size[1] - size[0] / new_aspect) /2)
+            img = ImageOps.expand(img, (border, 0), (0, 0, 0))
+        img = img.resize(new_size, Image.BILINEAR)
+        img.save('cuteness.png')
 
 def FS_dithering():
     # Floyd-Steinberg Dithering Algorithm
     # https://github.com/CodingTrain/website/blob/main/CodingChallenges/CC_090_dithering/Processing/CC_090_dithering/CC_090_dithering.pde
     factor = 1 # binary - black or white. pretty much - idk why not (maybe because of x/16.0?)
+    brightness_deviation = .02 # [0, .5), .02 is good
 
     with Image.open('cuteness.png') as img:
         pixels = img.load()
 
-        # TODO auto-expose. scale so max brightness is 1, min is 0 (for gray_pixel)
+        # gray scale the image and get darkest value
+        brightnesses = [] # brightness values for each pixel
         for x in range(img.size[0] - 1):
             for y in range(img.size[1] - 1):
                 (r, g, b) = pixels[x, y]
-                gray_pixel = int(math.sqrt(r*r + g*g + b*b))
+                pixel_brightness = (r + g + b) / 3
+                if (pixel_brightness != 0): brightnesses.append(pixel_brightness) # don't include pitch black
+                pixels[x, y] = (int(pixel_brightness), int(pixel_brightness), int(pixel_brightness))
+
+        # TODO set the map range of the pixels based on 'brightnesses' mean, median and/or mode
+        brightnesses.sort()
+        brightestish = brightnesses[int(len(brightnesses) * (1 - brightness_deviation)) - 1]
+        darkestish = brightnesses[int(len(brightnesses) * brightness_deviation)]
+        brightness_scale = 255 / (brightestish - darkestish)
+        
+        for x in range(img.size[0] - 1):
+            for y in range(img.size[1] - 1):
+                (r, g, b) = pixels[x, y]
+                gray_pixel = int((r - darkestish) * brightness_scale + darkestish) # only use r because it is already gray-scale
+                if gray_pixel > 255: gray_pixel = 255 # constrain
                 quantized_pixel = int(round(factor * gray_pixel / 255) * (255/factor))
 
                 pixels[x, y] = quantized_pixel
@@ -68,5 +95,12 @@ def FS_dithering():
                 neighbor_pixel = pixels[x+1, y+1][0]
                 new_neighbor_pixel = int(neighbor_pixel + diff * 1/16.0)
                 pixels[x+1, y+1] = (new_neighbor_pixel, new_neighbor_pixel, new_neighbor_pixel)
+       
+        # make completely black and white
+        for x in range(img.size[0]):
+            for y in range(img.size[1]):
+                (r, g, b) = pixels[x, y] # only use red value because they are all the same
+                quantized_pixel = int(round(r / 255) * 255)
+                pixels[x, y] = (quantized_pixel, quantized_pixel, quantized_pixel)
 
         img.save('cuteness.png')
